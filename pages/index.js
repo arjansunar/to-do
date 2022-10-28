536c545c1896eb5c2531eb5513627d8c04466724
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-10-28 09:42:35 +0545

    adding db and prisma models

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,38 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
+const initError = {
+  message: "",
+  err: false,
+};
 export default function Home() {
   const [user, setUser] = useState("");
-  const initError = {
-    message: "",
-    err: false,
-  };
   const [error, setError] = useState(initError);
 
-  const verify = (name) => {
-    typeof name === "string" && name.length >= 3;
-  };
+  const verify = useCallback(() => {
+    return user.length >= 3;
+  }, [user]);
 
-  const handleUserSubmission = (user) => {
-    if (!verify(user)) {
+  useEffect(() => {
+    const isValid = verify(user);
+
+    if (!isValid) {
       setError({
         message: "Name must be of more than 2 letters",
         err: true,
       });
+    } else {
+      setError(initError);
+    }
+  }, [user, verify]);
+
+  const handleUserSubmission = () => {
+    if (error.err) {
+      console.error(error);
+      return;
     }
     // call api after verification
+    console.log(user);
+    fetch();
   };
 
   return (
@@ -32,10 +45,13 @@ export default function Home() {
             type="text"
             placeholder="User name"
             className=" rounded-sm shadow-sm px-4 py-2 border border-gray-200 w-full mt-4"
-            onChange={(name) => setUser(name)}
+            onChange={(name) => setUser(name.target.value)}
             //  @keydown.enter="addTodo()"
           />
-          <button className="px-6 py-1   bg-black text-gray-100 rounded mt-5 max-w-fit self-end">
+          <button
+            onClick={handleUserSubmission}
+            className="px-6 py-1 bg-black text-gray-100 rounded mt-5 max-w-fit self-end"
+          >
             Go
           </button>
         </div>
